07e2594a93d1197a77556b95cdff82c3b81b3fee
Lafaiete <lafaietepedro3@gmail.com>
2024-05-29 19:02:15 -0300

    Second commit
    Front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import {ToastContainer} from "react-toastify";
+
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css"
+
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
